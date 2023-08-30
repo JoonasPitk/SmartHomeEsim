7c95e64e9c64a8970d26ee98debf8cee1cefcdc5
JoonasPitk <46161@edu.raseko.fi>
2023-08-30 14:57:33 +0300

    Initial running server and dummy data.

---
+++ app.js
@@ -0,0 +1,50 @@
+// WEB SERVER FOR ELECTRICITY USAGE PLANNING: WEB PAGES AND AN API
+// ==============================================================
+
+
+// LIBRARIES AND MODULES
+// Use Express as web engine
+const express = require("express");
+// Use Express Handlebars as template engine
+const {engine} = require("express-handlebars");
+
+
+// EXPRESS APPLICATION SETTINGS
+// Create the server
+const app = express();
+const PORT = process.env.PORT || 8080;
+
+// Set folder paths: public is for assets and views for pages
+app.use(express.static("public"));
+app.set("views", "./views");
+
+// Engine settings
+app.engine("handlebars", engine());
+app.set("view engine", "handlebars");
+
+
+// URL ROUTES
+
+// TODO: Add date and time as dynamic data for the homepage. Is it sensible to use server for creating time values?
+
+app.get("/", (req, res) => {
+    let homePageData = {
+        "price": 31.25, 
+        "wind": 2,
+        "temperature": 18,
+    };
+    res.render("index", homePageData)
+});
+
+app.get("/hourly", (req, res) => {
+    let hourlyData = {
+        "hour": 16,
+        "price": 13.67,
+    };
+    res.render("hourly", hourlyData)
+});
+
+
+// START THE LISTENER
+app.listen(PORT);
+console.log("Server started and it will listen TCP port", PORT);
